1f61f803607706b396f353088a69d63647edfb67
Prasannakumar <keeponfightingpj1995@gmail.com>
2021-05-26 02:25:42 +0530

    "Code Up"

--- lib/hello_world.rb
+++ lib/hello_world.rb
@@ -9,14 +9,8 @@ module HelloWorld
   # Your code goes here...
 
   def self.greet(url)
-    agent = Mechanize.new { |agent|
-      agent.user_agent_alias = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3'
-    }
-    agent.follow_meta_refresh = true
-    agent.follow_meta_refresh = true
-    #visit page
-    page = agent.get("#{url}")
-    puts page
+    doc = Nokogiri::HTML(open("#{url}"))
+    puts doc
   end
 end
 
