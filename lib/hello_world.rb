9aaa4009113b2c5c0a796fb2eaa94718ed84779c
Prasannakumar <keeponfightingpj1995@gmail.com>
2021-05-26 00:31:50 +0530

    "Updated Code !!!!!!"

--- lib/hello_world.rb
+++ lib/hello_world.rb
@@ -12,8 +12,8 @@ module HelloWorld
     # URI.open("http://www.ruby-lang.org/") {|f|
     #   f.each_line {|line| p line}
     # }
-
-    doc = Nokogiri::HTML(URI.open(url,"User-Agent"=> "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)"))
+    uri = URI.parse(url)
+    doc = Nokogiri::HTML(URI.open(uri,"User-Agent"=> "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)"))
     puts doc
     doc.xpath('//*[@id="ember530"]/div[2]/div[2]/div/div[1]/h1').each do |temp|
       puts temp
