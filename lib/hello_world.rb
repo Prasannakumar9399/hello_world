5001ae6d8bb6dee1a352c0a6dbafd236cfc4952c
Prasannakumar <keeponfightingpj1995@gmail.com>
2021-05-26 01:41:25 +0530

    "Updated Code !!!!!!!!!!!!!"

--- lib/hello_world.rb
+++ lib/hello_world.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 require 'nokogiri'
 require 'open-uri'
-require 'mechanize'
 require_relative "hello_world/version"
 
 module HelloWorld
@@ -9,10 +8,10 @@ module HelloWorld
   # Your code goes here...
 
   def self.greet(url)
-    puts url
-    agent = Mechanize.new
-    page = agent.get(url)
-    puts page
+    uri = URI.parse(url)
+    doc = Nokogiri::HTML(URI.open(uri,"User-agent"=> "Mozilla/5.0 (X11;U;Linux 2.4.2.-2 i586; en-us;m18) Gecko/200010131 Netscape6/6.01"))
+    puts doc
+
     end
   end
 
