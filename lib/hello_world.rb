f4daf4a85f428a68bc755cd4e849c0617330507b
Prasannakumar <keeponfightingpj1995@gmail.com>
2021-05-25 21:45:22 +0530

    "Updated Code"

--- lib/hello_world.rb
+++ lib/hello_world.rb
@@ -5,4 +5,8 @@ require_relative "hello_world/version"
 module HelloWorld
   class Error < StandardError; end
   # Your code goes here...
+
+  def self.greet(name)
+    puts "Hello, #{name}! I'm Ruby"
+  end
 end
