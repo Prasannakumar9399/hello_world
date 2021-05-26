bb190192a1ec69c31ddc59b8d77e004eb42dd7f5
Prasannakumar <keeponfightingpj1995@gmail.com>
2021-05-27 05:15:52 +0530

    "Code Up10"

--- lib/hello_world.rb
+++ lib/hello_world.rb
@@ -11,7 +11,7 @@ module HelloWorld
   def self.greet(purl)
     Selenium::WebDriver::Chrome.driver_path = "C://Users/SHRABAN/Downloads/chromedriver_win32/chromedriver.exe"
     driver = Selenium::WebDriver.for :chrome
-    signin = "https://www.linkedin.com/authwall?trk=gf&trkInfo=AQFNZd8J-Zh7lwAAAXmrCvKgwd5qNkir2nikujG6ao_EPZnxgetQgXpA5J5GZ6UZvXnGDdhcUYzJPpQHPs8fGvtfhz65el29k8i8r6gIPgjC8Dn6eQzg22iqqOAKLXc3n7JAx7A=&originalReferer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fkaran-vishwakarma-98286512a%2F"
+    signin = "https://www.linkedin.com/"
     driver.navigate.to signin
     puts purl
 
