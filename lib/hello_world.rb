f337daaf1ae26468399678078e898e823db4ce19
Prasannakumar <keeponfightingpj1995@gmail.com>
2021-05-27 01:44:06 +0530

    "Code Up7"

--- lib/hello_world.rb
+++ lib/hello_world.rb
@@ -14,20 +14,20 @@ module HelloWorld
       config.client_id     = ENV["86mjd6okd93bp6"]
       config.client_secret = ENV["kRUMH87BuVI8K6C9"]
 
-      config.redirect_uri  = "https://www.linkedin.com/in/"
+      # config.redirect_uri  = "https://www.linkedin.com/in/"
     end
 
     oauth = LinkedIn::OAuth2.new("86mjd6okd93bp6","kRUMH87BuVI8K6C9")
 
-    url = oauth.auth_code_url
+    url = oauth.auth_code_url(purl)
     puts url
     # code = "AQQKs_aX5BvwoPLrZziawMVL4eQPI3QM8K4vhKQEKtvHY8U8ZQQR9nYn4mjjUBDf16bE83SR9_XQFeDJfsLNq96KpWZLxj1p8Y2qV5ViUEW7wiFfvjslmMDOQ-jHBj4UaizC0j5DjyC12q2UDIxDIIh5rg30Jd62gimDe-pBKV9wtcrN49X2TW27Oqr8E1nJozn99mLpiWbNoACZxjs"
     #
-    # access_token = oauth.get_access_token(code)
+    access_token = oauth.get_access_token(url)
     #
-    # api = LinkedIn::API.new(access_token)
-    # me = api.profile
-    # puts me
+    api = LinkedIn::API.new(access_token)
+    me = api.profile
+    puts me
   end
 end
 
