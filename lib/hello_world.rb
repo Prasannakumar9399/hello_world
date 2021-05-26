23a1d532413a70956a52a18c77d289457c29f306
Prasannakumar <keeponfightingpj1995@gmail.com>
2021-05-27 03:37:50 +0530

    "Code Up10"

--- lib/hello_world.rb
+++ lib/hello_world.rb
@@ -10,28 +10,28 @@ module HelloWorld
 
 
   def self.greet(purl)
-    code = ""
-    if(code == nil)
-      LinkedIn.configure do |config|
-        config.client_id     = ENV["86mjd6okd93bp6"]
-        config.client_secret = ENV["kRUMH87BuVI8K6C9"]
-        config.scope = "r_liteprofile"
-        config.redirect_uri  = "https://www.linkedin.com/in/"
-      end
 
-      oauth = LinkedIn::OAuth2.new("86mjd6okd93bp6","kRUMH87BuVI8K6C9")
 
-      url = oauth.auth_code_url
-      puts url
+    LinkedIn.configure do |config|
+      config.client_id     = ENV["86mjd6okd93bp6"]
+      config.client_secret = ENV["kRUMH87BuVI8K6C9"]
+      config.scope = "r_liteprofile"
+      config.redirect_uri  = "https://www.linkedin.com/in/"
+    end
 
-    else
-    code = "AQQjYhlToCQF8g52WPVdqdnzepJH1jeDhrg1x3WRFUqOth646FER6gcwpJtZ0GwhuiWaTorIMVBhJ4NtZw7fFOnaDLoQpQXiBtcuX0xzbu6WWBpHDR5nR-LV2YBcOxci4QMrDrJy6JIhAWGtmb3gnLccI8ajx0liXz552zpCdX2Y_XIpYBh045XTdvs_vrypi_XdfK0Y-zWxzOa1Mso&state=ePhdJXSLXg2Wi3E0bZhDiyFnpI6IR4gTA%2F5VEMdgslg%3D"
-    end    # code = c;
-    access_token = oauth.get_access_token(code)
+    oauth = LinkedIn::OAuth2.new("86mjd6okd93bp6","kRUMH87BuVI8K6C9")
+
+    url = oauth.auth_code_url
+    puts url
+
+    # code = "AQQjYhlToCQF8g52WPVdqdnzepJH1jeDhrg1x3WRFUqOth646FER6gcwpJtZ0GwhuiWaTorIMVBhJ4NtZw7fFOnaDLoQpQXiBtcuX0xzbu6WWBpHDR5nR-LV2YBcOxci4QMrDrJy6JIhAWGtmb3gnLccI8ajx0liXz552zpCdX2Y_XIpYBh045XTdvs_vrypi_XdfK0Y-zWxzOa1Mso&state=ePhdJXSLXg2Wi3E0bZhDiyFnpI6IR4gTA%2F5VEMdgslg%3D"
     #
-    api = LinkedIn::API.new(access_token)
-    me = api.profile
-    puts me
+    # # code = c;
+    # access_token = oauth.get_access_token(code)
+    # #
+    # api = LinkedIn::API.new(access_token)
+    # me = api.profile
+    # puts me
   end
 end
 
