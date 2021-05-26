b3b6d46216cdf2d3867e440a87fad6cb1aad2fbe
Prasannakumar <keeponfightingpj1995@gmail.com>
2021-05-26 14:11:50 +0530

    "Code Up2"

--- lib/hello_world.rb
+++ lib/hello_world.rb
@@ -10,6 +10,7 @@ module HelloWorld
   def self.greet(url)
     URI.open("https://www.linkedin.com/in/?code=AQQKs_aX5BvwoPLrZziawMVL4eQPI3QM8K4vhKQEKtvHY8U8ZQQR9nYn4mjjUBDf16bE83SR9_XQFeDJfsLNq96KpWZLxj1p8Y2qV5ViUEW7wiFfvjslmMDOQ-jHBj4UaizC0j5DjyC12q2UDIxDIIh5rg30Jd62gimDe-pBKV9wtcrN49X2TW27Oqr8E1nJozn99mLpiWbNoACZxjs&state=someRandomString
 "){|f|}
+
     URI.open("#{url}") {|f|
       f.each_line {|line| p line}
     }
